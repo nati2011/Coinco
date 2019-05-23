e94978f1b6126ae6a8c874e5a67d178fee8a680e
Kayra Czerniewicz <kayracz@gmail.com>
2019-05-23 11:21:02 -0400

    errors fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/inicio'
   resources :products
-  resources :inicio
-  root to: 'page#inicio'
+  root to: 'home#inicio'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_233002) do
-
-  create_table "contactars", force: :cascade do |t|
-    t.string "categoria"
-    t.string "producto"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2019_05_20_232816) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
